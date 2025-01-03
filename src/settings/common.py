e667d7a13705790755640e17c9030a0cad4ed267
Carlos Caris <carlos.caris.m@gmail.com>
2025-01-02 23:09:56 -0300

    chore: add text embedding model setting

--- src/settings/common.py
+++ src/settings/common.py
@@ -9,7 +9,8 @@ __all__ = [
     'DENSE_EMBEDDINGS_DIMENSION',
     'SPARSE_EMBEDDINGS_MODEL',
     'QDRANT_COLLECTION_URL',
-    'QDRANT_API_KEY'
+    'QDRANT_API_KEY',
+    'TEXT_EMBEDDING_MODEL',
 ]
 
 
@@ -24,5 +25,9 @@ DEFAULT_PROMPT_PATH = "src/prompts/extraction_prompt copy.md"
 DEFAULT_FILE_PATH = "EETT Infranormativo JI Los Palomos_rev02_normativo-print.pdf"
 
 # Qdrant settings
-QDRANT_COLLECTION_URL = "https://2abb314c-18b3-48ed-8bb3-4594a30c6ca1.us-east4-0.gcp.cloud.qdrant.io"
+QDRANT_COLLECTION_URL = "ht tps://2abb314c-18b3-48ed-8bb3-4594a30c6ca1.us-east4-0.gcp.cloud.qdrant.io"
 QDRANT_API_KEY = "K8337dWIuSN6dOr1t8HYQH0MU0mctqqJSzcF306eSuJzHLUvsSxZew"
+
+# Text embedding settings
+TEXT_EMBEDDING_MODEL = "text-embedding-3-small"
+
